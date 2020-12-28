86dec8dae222fcbe84753b0d3e420eeb128afd1f
Darius Robertson <robertsond3@vcu.edu>
2020-12-28 16:16:49 -0500

    small changes

--- app/src/main/java/com/example/socialnetwork/MainActivity.java
+++ app/src/main/java/com/example/socialnetwork/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.socialnetwork;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.graphics.drawable.Icon;
@@ -16,26 +19,42 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
     private Button homeButton;
     private Button messagesButton;
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawer = findViewById(R.id.drawer_layout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
 
-        //Toolbar toolbar = findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+        /***ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawer,toolbar,
+               R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+         toggle.syncState();
+         ***/
+
 
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.action_bar_layout);
-       // messagesButton = findViewById(R.id.messages);
-       // homeButton = findViewById(R.id.homeButton);
 
 
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.main, menu);
         return super.onCreateOptionsMenu(menu);
+
+
 
     }
 }

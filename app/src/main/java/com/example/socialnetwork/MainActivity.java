f9daf14a4431401a665f2af725e026e6f4c6c67d
Darius Robertson <robertsond3@vcu.edu>
2020-12-27 23:23:55 -0500

    added navigation drawer to main activity

--- app/src/main/java/com/example/socialnetwork/MainActivity.java
+++ app/src/main/java/com/example/socialnetwork/MainActivity.java
@@ -1,19 +1,36 @@
 package com.example.socialnetwork;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.graphics.drawable.Icon;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button homeButton;
+    private Button messagesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.action_bar_layout);
+       // messagesButton = findViewById(R.id.messages);
+       // homeButton = findViewById(R.id.homeButton);
+
+
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
